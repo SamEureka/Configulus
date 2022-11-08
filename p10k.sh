2f70fd0e36b276c218d65a57c9fcf97e64029dc7
Sam Dennon <sameureka@me.com>
2022-11-08 15:36:08 -0800

    Update p10k.sh

--- p10k.sh
+++ p10k.sh
@@ -3,7 +3,21 @@
 ## P10k Install Script
 ## Sam Dennon // 2022
 
-echo "What is your github.com username?"
-read GH_USERNAME
-echo "What email do you use for github.com?"
-read GH_EMAIL
+echo "We need to collect some info from you."
+read -p 'Github username: ' GH_USERNAME
+read -p 'Github email address: ' GH_EMAIL
+read -p 'Your username on this system: ' USER
+
+sudo chsh -s /usr/bin/zsh $USER
+
+git config --global user.name $GH_USERNAME
+git config --global user.email $GH_EMAIL
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.zsh/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+tee -a ~/.zshrc << EOF
+source ~/.zsh/powerlevel10k/powerlevel10k.zsh-theme
+source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+EOF
+exec zsh
