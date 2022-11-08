92282bd6855fd0525a6d7dd77518bbe1de09c551
Sam Dennon <sameureka@me.com>
2022-11-08 14:53:32 -0800

    Create p10k.sh

---
+++ p10k.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+## P10k Install Script
+## Sam Dennon // 2022
+
+echo "What is your github.com username?"
+read GH_USERNAME
+echo "What email do you use for github.com?"
+read GH_EMAIL
