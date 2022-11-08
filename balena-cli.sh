1815d7d2c99de473debf2e0e0d06f71e8a1145cc
Sam Dennon <sameureka@me.com>
2022-11-08 11:31:59 -0800

    Update balena-cli.sh

--- balena-cli.sh
+++ balena-cli.sh
@@ -14,14 +14,16 @@ wget -qO /usr/local/share/balena-cli.zip \
 "https://github.com/balena-io/balena-cli/releases/download/${BALENA_CLI_VERSION}/balena-cli-${BALENA_CLI_VERSION}-linux-x64-standalone.zip"
 unzip -qq -o /usr/local/share/balena-cli.zip -d /usr/local/share/
 rm /usr/local/share/balena-cli.zip
-if [[ -L "/usr/bin/balena-cli" ]]; then
-  if [[ -e "/usr/bin/balena" ]]; then
-    echo "balena-cli is linked"
-  else
-    echo "rebuilding balena-cli link"
-    unlink /usr/bin/balena
-    ln -s /usr/local/share/balena-cli/balena /usr/bin/balena
-  fi
+if [[ -L "/usr/bin/balena" ]]
+  then
+    if [[ -e "/usr/bin/balena" ]]
+      then
+        echo "balena-cli is linked"
+    else
+      echo "rebuilding balena-cli link"
+      unlink /usr/bin/balena
+      ln -s /usr/local/share/balena-cli/balena /usr/bin/balena
+    fi
 else
   echo "linking balena-cli"
   ln -s /usr/local/share/balena-cli/balena /usr/bin/balena
