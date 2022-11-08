d383ba49dd89cc25c14d0eabbd23977f1b300e97
Sam Dennon <sameureka@me.com>
2022-11-08 11:14:25 -0800

    more link checking code
    Change-type: patch
    Signed-off-by: Sam Dennon <sam@dennon.me>

--- balena-cli.sh
+++ balena-cli.sh
@@ -3,9 +3,11 @@
 ## balena-cli install script
 ## Sam Dennon // 2022
 
+VERSION=v14.5.2
+
 # Set the CLI version
 echo "INstalling balena-cli"
-BALENA_CLI_VERSION="${BALENA_CLI_VERSION:-v14.5.2}"
+BALENA_CLI_VERSION="${BALENA_CLI_VERSION:-${VERSION}}"
 sudo -s -- <<EOF
 mkdir -p /usr/local/share/balena-cli
 wget -qO /usr/local/share/balena-cli.zip \
@@ -13,10 +15,16 @@ wget -qO /usr/local/share/balena-cli.zip \
 unzip -qq -o /usr/local/share/balena-cli.zip -d /usr/local/share/
 rm /usr/local/share/balena-cli.zip
 if [[ -L "/usr/bin/balena-cli" ]]; then
-  echo "balena-cli is already linked"
+  if [[ -e "/usr/bin/balena" ]]; then
+    echo "balena-cli is linked"
+  else
+  echo "rebuilding balena-cli link"
+    unlink /usr/bin/balena
+    ln -s /usr/local/share/balena-cli/balena /usr/bin/balena
 else
   echo "linking balena-cli"
   ln -s /usr/local/share/balena-cli/balena /usr/bin/balena
 fi
 echo "balena-cli installed"
+balena -h
 EOF
