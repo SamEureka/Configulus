cb5fe956b87f51cee08a4105cba97ddaf927a34d
Sam Dennon <sameureka@me.com>
2022-11-08 09:07:38 -0800

    created balena-cli.sh
    Change-type: major
    Signed-off-by: Sam Dennon <sam@dennon.me>

---
+++ balena-cli.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+## balena-cli install script
+## Sam Dennon // 2022
+
+# Set the CLI version
+BALENA_CLI_VERSION="${BALENA_CLI_VERSION:-v14.3.1}"
+sudo -s -- <<EOF
+mkdir -p /usr/local/share/balena-cli
+wget -qO /usr/local/share/balena-cli.zip \
+"https://github.com/balena-io/balena-cli/releases/download/${BALENA_CLI_VERSION}/balena-cli-${BALENA_CLI_VERSION}-linux-x64-standalone.zip"
+unzip -qq -o /usr/local/share/balena-cli.zip -d /usr/local/share/
+rm /usr/local/share/balena-cli.zip
+ln -s /usr/local/share/balena-cli/balena /usr/bin/balena
+echo "balena-cli installed"
+EOF
