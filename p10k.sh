c508c8691e3e0fae7cf0f60933565989be4e0484
Sam Dennon <sameureka@me.com>
2022-11-09 05:24:41 -0800

    change from tee to cat

--- p10k.sh
+++ p10k.sh
@@ -15,7 +15,7 @@ git config --global user.email $GH_EMAIL
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.zsh/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
-tee -a ~/.zshrc << EOF
+cat << EOF >> ~/.zshrc
 source ~/.zsh/powerlevel10k/powerlevel10k.zsh-theme
 source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
 source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
