9516528da64928cf81e8a1ac8fd3c48b4a3fa346
Sam Dennon <sameureka@me.com>
2022-11-09 09:03:04 -0800

    added a info confirm and font check set
    Change-type:  patch
    Signed-off-by: Sam Dennnon <sam@dennon.me>

--- p10k.sh
+++ p10k.sh
@@ -7,6 +7,33 @@ echo "We need to collect some info from you."
 read -p 'Github username: ' GH_USERNAME
 read -p 'Github email address: ' GH_EMAIL
 read -p 'Your username on this system: ' USER
+echo " "
+echo "Is this info correct?"
+echo " "
+echo "Github username: $GH_USERNAME"
+echo "Github email address: $GH_EMAIL"
+echo "System username: $USER"
+read -p '(y)es / (n)o: ' ANSWER
+
+case $ANSWER in
+  y | yes | Y | YES | 1 | Yes)
+    echo "Cool! Let's do this!";;
+  n | no | N | NO | 0 | No)
+    echo "Exiting, try running the script again."
+    exit 1;;    
+  *)
+    echo "Answer not understood, try 'y' or 'n'. exiting."
+    exit 1;;
+esac
+
+if [ -e "/usr/share/fonts/nerd/Operator Mono Lig Book NF.otf" ]
+  then
+    echo "Setting Gnome monospace font to Operator Mono Nerd Font"
+    gsettings set org.gnome.desktop.interface monospace-font-name 'OperatorMonoLig Nerd Font 14'
+  else
+    echo 'Nerd fonts not installed. Please run `sh -c "$(wget -O- https://bit.ly/NerdFonts)"` to install.'
+    exit 1
+fi
 
 sudo chsh -s /usr/bin/zsh $USER
 
