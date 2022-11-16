2f799c6ad5933d89f98d90aebeeea292cc443005
Sam Dennon <sameureka@me.com>
2022-11-16 11:56:01 -0800

    Update NerdFonts.sh

--- NerdFonts.sh
+++ NerdFonts.sh
@@ -13,17 +13,17 @@ fi
 
 sudo -s -- <<EOF
 mkdir -p /usr/share/fonts/nerd
-echo "Downloading Font zip one..."
+echo "Downloading Font zip one."
 wget -qO /usr/share/fonts/nerd/NF1.zip "http://bit.ly/3DVVyLx"
-echo "Downloading Font zip two..."
+echo "Downloading Font zip two."
 wget -qO /usr/share/fonts/nerd/NF2.zip "http://bit.ly/3A2uqtf"
-echo "Downloading Font zip three..."
+echo "Downloading Font zip three."
 wget -qO /usr/share/fonts/nerd/NF3.zip "http://bit.ly/3fOowVM"
-echo "Downloading Font zip four..."
+echo "Downloading Font zip four."
 wget -qO /usr/share/fonts/nerd/NF4.zip "http://bit.ly/3TlMk11"
-echo "Downloading Font zip five..."
+echo "Downloading Font zip five."
 wget -qO /usr/share/fonts/nerd/NF5.zip "http://bit.ly/3GabpsC"
-echo "un-zzzzzzzzzzzzzzzzzip......"
+echo "un-zzzzzzzzzzzzzzzzzip"
 unzip -qj "/usr/share/fonts/nerd/*.zip" -d /usr/share/fonts/nerd
 rm /usr/share/fonts/nerd/*.zip
 echo "Rebuilding the font cache."
