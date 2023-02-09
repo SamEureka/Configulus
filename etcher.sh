d4294c858ffed976cc2019e22ac2d8b261e9fe77
Sam Dennon <sameureka@me.com>
2023-02-09 15:25:14 -0800

    Update etcher.sh

--- etcher.sh
+++ etcher.sh
@@ -3,4 +3,4 @@
 curl -1sLf \
    'https://dl.cloudsmith.io/public/balena/etcher/setup.deb.sh' \
    | sudo -E bash
-sudo apt update && sudo apt install-y balena-etcher-electron
+sudo apt update && sudo apt install -y balena-etcher-electron
